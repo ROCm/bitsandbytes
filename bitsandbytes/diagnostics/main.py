49c10f7cd38a9c32c625ac2c28396022a21d7d07
MISHANMAURYA <118961433+MISHANMAURYA@users.noreply.github.com>
2025-06-03 21:26:18 +0530

    Update main.py

--- bitsandbytes/diagnostics/main.py
+++ bitsandbytes/diagnostics/main.py
@@ -5,14 +5,14 @@ import traceback
 
 import torch
 
-from bitsandbytes import __version__ as bnb_version  
-from bitsandbytes.cextension import BNB_BACKEND, HIP_ENVIRONMENT  
-from bitsandbytes.consts import PACKAGE_GITHUB_URL  
-from bitsandbytes.cuda_specs import get_cuda_specs  
-from bitsandbytes.diagnostics.cuda import (  
-    print_diagnostics,  
-    print_runtime_diagnostics,  
-)  
+from bitsandbytes import __version__ as bnb_version
+from bitsandbytes.cextension import BNB_BACKEND, HIP_ENVIRONMENT
+from bitsandbytes.consts import PACKAGE_GITHUB_URL
+from bitsandbytes.cuda_specs import get_cuda_specs
+from bitsandbytes.diagnostics.cuda import (
+    print_diagnostics,
+    print_runtime_diagnostics,
+)
 from bitsandbytes.diagnostics.utils import print_dedented, print_header
 
 _RELATED_PACKAGES = [
@@ -71,51 +71,51 @@ def show_environment():
         print(f"  {pkg}: {version}")
 
 
-def main():  
-    print_header(f"bitsandbytes v{bnb_version}")  
-    show_environment()  
-    print_header("")  
-  
-    cuda_specs = get_cuda_specs()  
-    if HIP_ENVIRONMENT:  
-        rocm_specs = f" rocm_version_string='{cuda_specs.cuda_version_string}',"  
-        rocm_specs += f" rocm_version_tuple={cuda_specs.cuda_version_tuple}"  
-        print(f"{BNB_BACKEND} specs:{rocm_specs}")  
-    else:  
-        print(f"{BNB_BACKEND} specs:{cuda_specs}")  
-    if not torch.cuda.is_available():  
-        print(f"Torch says {BNB_BACKEND} is not available. Possible reasons:")  
-        if not HIP_ENVIRONMENT: print(f"- {BNB_BACKEND} driver not installed")  
-        print(f"- {BNB_BACKEND} not installed")  
-        print(f"- You have multiple conflicting {BNB_BACKEND} libraries")  
-    if cuda_specs:  
-        print_diagnostics(cuda_specs)  
-    print_runtime_diagnostics()  
-    print_header("")  
-    print_header("DEBUG INFO END")  
-    print_header("")  
-    print(f"Checking that the library is importable and {BNB_BACKEND} is callable...")  
-    try:  
-        sanity_check()  
-        print("SUCCESS!")  
-        print("Installation was successful!")  
-        return  
-    except RuntimeError as e:  
-        if "not available in CPU-only" in str(e):  
-            print(  
-                f"WARNING: {__package__} is currently running as CPU-only!\n"  
-                "Therefore, 8-bit optimizers and GPU quantization are unavailable.\n\n"  
-                f"If you think that this is so erroneously,\nplease report an issue!",  
-            )  
-        else:  
-            raise e  
-    except Exception:  
-        traceback.print_exc()  
-    print_dedented(  
-        f"""  
-        Above we output some debug information.  
-        Please provide this info when creating an issue via {PACKAGE_GITHUB_URL}/issues/new/choose  
-        WARNING: Please be sure to sanitize sensitive info from the output before posting it.  
-        """,  
-    )  
-    sys.exit(1)  
+def main():
+    print_header(f"bitsandbytes v{bnb_version}")
+    show_environment()
+    print_header("")
+    
+    cuda_specs = get_cuda_specs()
+    if HIP_ENVIRONMENT:
+        rocm_specs = f" rocm_version_string='{cuda_specs.cuda_version_string}',"
+        rocm_specs += f" rocm_version_tuple={cuda_specs.cuda_version_tuple}"
+        print(f"{BNB_BACKEND} specs:{rocm_specs}")
+    else:
+        print(f"{BNB_BACKEND} specs:{cuda_specs}")
+    if not torch.cuda.is_available():
+        print(f"Torch says {BNB_BACKEND} is not available. Possible reasons:")
+        if not HIP_ENVIRONMENT: print(f"- {BNB_BACKEND} driver not installed")
+        print(f"- {BNB_BACKEND} not installed")
+        print(f"- You have multiple conflicting {BNB_BACKEND} libraries")
+    if cuda_specs:
+        print_diagnostics(cuda_specs)
+    print_runtime_diagnostics()
+    print_header("")
+    print_header("DEBUG INFO END")
+    print_header("")
+    print(f"Checking that the library is importable and {BNB_BACKEND} is callable...")
+    try:
+        sanity_check()
+        print("SUCCESS!")
+        print("Installation was successful!")
+        return
+    except RuntimeError as e:
+        if "not available in CPU-only" in str(e):
+            print(
+                f"WARNING: {__package__} is currently running as CPU-only!\n"
+                "Therefore, 8-bit optimizers and GPU quantization are unavailable.\n\n"
+                f"If you think that this is so erroneously,\nplease report an issue!",
+            )
+        else:
+            raise e
+    except Exception:
+        traceback.print_exc()
+    print_dedented(
+        f"""
+        Above we output some debug information.
+        Please provide this info when creating an issue via {PACKAGE_GITHUB_URL}/issues/new/choose
+        WARNING: Please be sure to sanitize sensitive info from the output before posting it.
+        """,
+    )
+    sys.exit(1)
